90d7dc5683eef27ceb941f0dc93cbbdd9ff13ef4
mankarth004 <mankarth004@gmail.com>
2024-01-05 21:53:17 +0530

    Initial Commit

---
+++ FrameworkSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FrameworkSample
+//
+//  Created by kartheek.manthoju on 05/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FrameworkSample/FrameworkSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworkSampleApp.swift
+//  FrameworkSample
+//
+//  Created by kartheek.manthoju on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FrameworkSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
