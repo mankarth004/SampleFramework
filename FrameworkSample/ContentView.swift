66e55e331c0cc551c0c6d197e437f06695c434d8
mankarth004 <mankarth004@gmail.com>
2024-01-09 01:02:08 +0530

    Framework added

--- FrameworkSample/ContentView.swift
+++ FrameworkSample/ContentView.swift
@@ -6,14 +6,17 @@
 //
 
 import SwiftUI
+import KMUI
 
 struct ContentView: View {
+    @State var input = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            KMTextField(input: $input, placeholder: "Enter name here", borderColor: .red, borderWidth: 2, cornerRadius: 10, fontSize: <#T##CGFloat#>)
         }
         .padding()
     }
