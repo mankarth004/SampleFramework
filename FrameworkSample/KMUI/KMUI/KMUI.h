22ae24f9f3d30811524358bcbcbfb191202a8b2f
mankarth004 <mankarth004@gmail.com>
2024-01-09 01:25:16 +0530

    Framework added

---
+++ FrameworkSample/KMUI/KMUI/KMUI.h
@@ -0,0 +1,18 @@
+//
+//  KMUI.h
+//  KMUI
+//
+//  Created by kartheek.manthoju on 09/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KMUI.
+FOUNDATION_EXPORT double KMUIVersionNumber;
+
+//! Project version string for KMUI.
+FOUNDATION_EXPORT const unsigned char KMUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMUI/PublicHeader.h>
+
+
